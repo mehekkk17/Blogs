d81aa4c150f6afdc71fdc726a404c78932272f08
Mehak Mahajan <mehak.mahajan@ringcentral.com>
2026-02-17 21:18:55 +0530

    merge conflicts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,4 +5,10 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
   config.public_file_server.enabled = true
+  config.log_level = :info
+  # Log to stdout so Render (and similar hosts) show errors in the dashboard
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    config.logger = ActiveSupport::Logger.new($stdout)
+    config.logger.formatter = config.log_formatter
+  end
 end
